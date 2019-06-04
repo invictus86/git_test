11d01441d7fd7d05117e0579fb61e0c9a5114856
invictus86 <1519517487@qq.com>
2019-06-04 08:36:07 +0800

    add

--- opencv_test.py
+++ opencv_test.py
@@ -1,2 +1,8 @@
 import cv2
-img = cv2.imread("")
+
+img = cv2.imread("G:/code/git_test/1.jpg", cv2.IMREAD_GRAYSCALE)
+cv2.namedWindow("Demo")
+cv2.startWindowThread()
+cv2.imshow("Demo", img)
+cv2.waitKey(0)
+cv2.destoryAllWindows()
